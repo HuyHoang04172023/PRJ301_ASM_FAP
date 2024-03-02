bf601a9a21d8e876d72f803ccdb962bd286c9af0
HuyHoang04172023 <125451388+HuyHoang04172023@users.noreply.github.com>
2024-03-02 23:44:47 +0700

    LessionDBContext
    Write again sql
    getSessionByLecturerIdFromTo()
    Finish getSessionByLecturerIdFromTo()

--- FAP_FPT_ASM/src/java/dal/SessionDBContext.java
+++ FAP_FPT_ASM/src/java/dal/SessionDBContext.java
@@ -4,7 +4,12 @@
  */
 package dal;
 
+import entity.Group;
+import entity.Lecturer;
+import entity.Room;
 import entity.Session;
+import entity.Subject;
+import entity.TimeSlot;
 import java.util.ArrayList;
 import java.sql.*;
 import java.util.logging.Level;
@@ -19,35 +24,62 @@ public class SessionDBContext extends DBContext<Session> {
     public ArrayList<Session> getSessionByLecturerIdFromTo(String lid, Date from, Date to) {
         ArrayList<Session> sessions = new ArrayList<>();
         try {
-            String sql = "SELECT \n"
+            String sql = "SELECT"
                     + "ses.sesid,ses.date,ses.istaken,\n"
-                    + "g.gid,g.gname,g.startdate,g.tfirst,g.tsecond,\n"
-                    + "g.dayfirst,g.daysecond,g.totalslot,\n"
-                    + "s.subid,s.subname,s.credit,\n"
+                    + "g.gid,g.gname,\n"
+                    + "s.subid,s.subname,\n"
                     + "r.rid,r.rnumber,\n"
                     + "t.tid,t.tname,t.tbegin,t.tend,\n"
-                    + "l.lid,l.lname,l.lage,l.lgender,l.lemail\n"
+                    + "l.lid,l.lname\n"
                     + "FROM Session ses INNER JOIN [Group] g ON ses.gid = g.gid\n"
-                    + "				 INNER JOIN Subject s ON g.subid = s.subid\n"
-                    + "				 INNER JOIN Room r ON ses.rid = r.rid\n"
-                    + "				 INNER JOIN TimeSlot t ON ses.tid = t.tid\n"
-                    + "				 INNER JOIN Lecturer l ON ses.lid = l.lid\n"
+                    + "                 INNER JOIN Subject s ON g.subid = s.subid\n"
+                    + "                 INNER JOIN Room r ON ses.rid = r.rid\n"
+                    + "                 INNER JOIN TimeSlot t ON ses.tid = t.tid\n"
+                    + "                 INNER JOIN Lecturer l ON ses.lid = l.lid\n"
                     + "WHERE ses.lid = '?' AND ses.date BETWEEN '?' AND '?'";
             PreparedStatement stm = connection.prepareStatement(sql);
             stm.setString(1, lid);
             stm.setDate(2, from);
             stm.setDate(3, to);
             ResultSet rs = stm.executeQuery();
-            while (rs.next()) {  
+            while (rs.next()) {
+                Session ses = new Session();
+                Group g = new Group();
+                Subject sub = new Subject();
+                Room r = new Room();
+                TimeSlot t = new TimeSlot();
+                Lecturer l = new Lecturer();
                 
-                Session s = new Session();
                 
                 
                 
+                ses.setId(rs.getInt("sesid"));
+                ses.setDate(rs.getDate("date"));
+                ses.setIsTaken(rs.getBoolean("istaken"));
                 
+                g.setId(rs.getInt("gid"));
+                g.setName(rs.getString("gname"));
+                sub.setId(rs.getInt("subid"));
+                sub.setName(rs.getString("subname"));
+                g.setSubject(sub);
+                ses.setGroup(g);
+                
+                r.setId(rs.getInt("rid"));
+                r.setName(rs.getString("rnumber"));
+                ses.setRoom(r);
+                
+                t.setId(rs.getInt("tid"));
+                t.setName(rs.getString("tname"));
+                t.setBegin(rs.getTimestamp("tbegin"));
+                t.setEnd(rs.getTimestamp("tend"));
+                ses.setSlot(t);
+                
+                l.setId(rs.getString("lid"));
+                l.setName(rs.getString("lname"));
+                ses.setLecturer(l);
+                sessions.add(ses);
             }
-            
-            
+
         } catch (SQLException ex) {
             Logger.getLogger(SessionDBContext.class.getName()).log(Level.SEVERE, null, ex);
         }
